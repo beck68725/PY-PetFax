b59a4136065acb9ecd5274af9d10b3a3b936f3c2
Rebecca Coburn <rebeccacoburn@Rebeccas-iMac.local>
2022-10-01 13:28:05 -0500

    activity 6-2 complete

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_migrate import Migrate
 
 # factory
 
@@ -6,7 +7,15 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
+
     # index route
+
     @app.route('/')
     def index():
         return 'Hello, PetFax!'
